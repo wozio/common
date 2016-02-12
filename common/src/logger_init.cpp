5899460f49ed0c7baa1be7bfd8403f8ad15790cf
Piotr Wozniak <woziosek@gmail.com>
2016-02-12 13:23:18 +0100

    New netBeans projects. Logging to file only enabled when on linux with /var/log/home-system directory created

--- common/src/logger_init.cpp
+++ common/src/logger_init.cpp
@@ -8,17 +8,29 @@ namespace home_system
 
 void init_log(const char* file, bool console_log)
 {
+#ifdef __linux__
   std::string path("/var/log/home-system/");
-  boost::filesystem::create_directories(path);
+  std::string to_file("true");
+  try
+  {
+    boost::filesystem::create_directories(path);
+  }
+  catch (...)
+  {
+    to_file = "false";
+  }
+#else
+  to_file = "false";
+#endif
   
   el::Configurations defaultConf;
   defaultConf.setToDefault();
   defaultConf.setGlobally(el::ConfigurationType::Filename, path + file);
   defaultConf.setGlobally(el::ConfigurationType::MaxLogFileSize, std::to_string(1 * 1024 * 1024));
   defaultConf.setGlobally(el::ConfigurationType::LogFlushThreshold, "10");
-  defaultConf.setGlobally(el::ConfigurationType::ToFile, "true");
+  defaultConf.setGlobally(el::ConfigurationType::ToFile, to_file);
   defaultConf.setGlobally(el::ConfigurationType::ToStandardOutput, console_log ? "true" : "false");
-  defaultConf.setGlobally(el::ConfigurationType::Format, "[%datetime] [%levshort] [%thread] [%loc] %msg");
+  defaultConf.setGlobally(el::ConfigurationType::Format, "[%datetime] [%levshort] [%thread] [%fbase:%line] %msg");
   el::Loggers::reconfigureLogger("default", defaultConf);
   el::Loggers::addFlag(el::LoggingFlag::StrictLogFileSizeCheck);
 }
