e3612e57617dbc6c47ef43b61a1e41f0a4e21d4e
wozio <woziosek@gmail.com>
2016-03-06 10:34:03 +0100

    handler_t.h added

---
+++ wshandling/handler_t.h
@@ -0,0 +1,14 @@
+#ifndef HANDLER_T_H
+#define HANDLER_T_H
+
+#include <memory>
+
+namespace home_system
+{
+
+  class handler;
+  typedef std::shared_ptr<handler> handler_t;
+
+}
+
+#endif /* HANDLER_T_H */
